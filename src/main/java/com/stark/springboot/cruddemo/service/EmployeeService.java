390d1a123a12a9b311227b2463d7a5b9b81fe59b
Oyedotun Sodiq <oyedotunsodiq045@gmail.com>
2019-04-18 17:50:48 +0000

    tested gitpod live ide

--- src/main/java/com/stark/springboot/cruddemo/service/EmployeeService.java
+++ src/main/java/com/stark/springboot/cruddemo/service/EmployeeService.java
@@ -1,5 +1,6 @@
 /*
  * @author sodiq oyedotun stark
+ * modified 18/4/2019
  */
 package com.stark.springboot.cruddemo.service;
 
@@ -8,13 +9,13 @@ import java.util.List;
 import com.stark.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeService {
-	
+
 	public List<Employee> findAll();
-	
+
 	public Employee findById(int theId);
-	
+
 	public void save(Employee theEmployee);
-	
+
 	public void deleteById(int theId);
 
 }
